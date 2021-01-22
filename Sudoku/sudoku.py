4708364147d03dbe9610e605eb21342301fc1e45
Harsh Khilawala <harsh.khilawala27@gmail.com>
2021-01-22 19:03:28 +0530

    performed elimination on grid values

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -13,10 +13,16 @@ unitlist = row_units + column_units + square_units
 units = dict((s,[u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s,set(sum(units[s],[]))-set([s])) for s in boxes)
 
-
+values=[]
+digits='123456789'
 def grid_values(grid):
     assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
-    return dict(zip(boxes,grid))
+    for i in grid:
+        if i == '.':
+            values.append(digits)
+        else:
+            values.append(i)
+    return dict(zip(boxes,values))
 
 def display(values):
     """
@@ -32,7 +38,24 @@ def display(values):
         if r in 'CF': print(line)
     return
 
+def eliminate(values):
+    solved_values = [box for box in values.keys() if len(values[box])==1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+def only_choice(values):
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces)==1:
+                values[dplaces[0]]= digit
+    return values
 
 grid= '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 values= grid_values(grid)
+values=eliminate(values)
+values = only_choice(values)
 display(values)
